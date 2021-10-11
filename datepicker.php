c4a9dae515e935e240afcfbfcc8b16f0707f1124
osgun4christ <osgun4christ@gmail.com>
2021-10-11 00:58:49 +0000

    Date_picker_dropdown

---
+++ datepicker.php
@@ -0,0 +1,127 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+<form>
+        <span>
+            <label for="day">Day:</label>
+            <select name="day" id="day"></select>
+        </span>
+        <span>
+            <label for="month">Month:</label>
+            <select name="month" id="month"></select>
+        </span>
+        <span>
+            <label for="year">Year:</label>
+            <select name="year" id="year">Year:</select>
+        </span>
+    </form>
+</body>
+
+<script>
+//Create references to the dropdown's
+const yearSelect = document.getElementById("year");
+const monthSelect = document.getElementById("month");
+const daySelect = document.getElementById("day");
+
+const months = ['January', 'February', 'March', 'April', 
+'May', 'June', 'July', 'August', 'September', 'October',
+'November', 'December'];
+
+//Months are always the same
+(function populateMonths(){
+    for(let i = 0; i < months.length; i++){
+        const option = document.createElement('option');
+        option.textContent = months[i];
+        monthSelect.appendChild(option);
+    }
+    monthSelect.value = "January";
+})();
+
+let previousDay;
+
+function populateDays(month){
+    //Delete all of the children of the day dropdown
+    //if they do exist
+    while(daySelect.firstChild){
+        daySelect.removeChild(daySelect.firstChild);
+    }
+    //Holds the number of days in the month
+    let dayNum;
+    //Get the current year
+    let year = yearSelect.value;
+
+    if(month === 'January' || month === 'March' || 
+    month === 'May' || month === 'July' || month === 'August' 
+    || month === 'October' || month === 'December') {
+        dayNum = 31;
+    } else if(month === 'April' || month === 'June' 
+    || month === 'September' || month === 'November') {
+        dayNum = 30;
+    }else{
+        //Check for a leap year
+        if(new Date(year, 1, 29).getMonth() === 1){
+            dayNum = 29;
+        }else{
+            dayNum = 28;
+        }
+    }
+    //Insert the correct days into the day <select>
+    for(let i = 1; i <= dayNum; i++){
+        const option = document.createElement("option");
+        option.textContent = i;
+        daySelect.appendChild(option);
+    }
+    if(previousDay){
+        daySelect.value = previousDay;
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 1;
+        }
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 2;
+        }
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 3;
+        }
+    }
+}
+
+function populateYears(){
+    //Get the current year as a number
+    let year = new Date().getFullYear();
+    //Make the previous 100 years be an option
+    for(let i = 0; i < 101; i++){
+        const option = document.createElement("option");
+        option.textContent = year - i;
+        yearSelect.appendChild(option);
+    }
+}
+
+populateDays(monthSelect.value);
+populateYears();
+
+yearSelect.onchange = function() {
+    populateDays(monthSelect.value);
+}
+monthSelect.onchange = function() {
+    populateDays(monthSelect.value);
+}
+daySelect.onchange = function() {
+    previousDay = daySelect.value;
+}
+</script>
+<!-- credit -->
+<!-- Custom Date Dropdown Picker
+Luke F. Courtney -->
+</html>
